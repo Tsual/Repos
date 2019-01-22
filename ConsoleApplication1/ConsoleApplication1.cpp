3d0ccd4c9a6b02f4e6404312d5d38279a294d1be
张良 <562974055@qq.com>
2019-01-22 10:13:20 +0800

    添加项目文件。

---
+++ ConsoleApplication1/ConsoleApplication1.cpp
@@ -0,0 +1,80 @@
+﻿// ConsoleApplication1.cpp : 此文件包含 "main" 函数。程序执行将在此处开始并结束。
+//
+
+#include "pch.h"
+#include "aes.h"
+#include "filters.h"
+#include "secblock.h"
+#include "hex.h"
+#include "modes.h"
+
+using namespace std;
+using namespace CryptoPP;
+
+string AES_CTR_Encrypt(const char *, const char *);
+
+
+int main()
+{
+	cout << "Hello World!\n";
+	string* str = new string("123456");
+	unsigned char* seq0 = (unsigned char*)str->c_str();
+
+	cout << seq0 << "\n";
+
+	auto rtn=AES_CTR_Encrypt("123456789012", "123456");
+
+
+
+
+
+
+
+
+
+
+
+
+}
+
+// 运行程序: Ctrl + F5 或调试 >“开始执行(不调试)”菜单
+// 调试程序: F5 或调试 >“开始调试”菜单
+
+// 入门提示: 
+//   1. 使用解决方案资源管理器窗口添加/管理文件
+//   2. 使用团队资源管理器窗口连接到源代码管理
+//   3. 使用输出窗口查看生成输出和其他消息
+//   4. 使用错误列表窗口查看错误
+//   5. 转到“项目”>“添加新项”以创建新的代码文件，或转到“项目”>“添加现有项”以将现有代码文件添加到项目
+//   6. 将来，若要再次打开此项目，请转到“文件”>“打开”>“项目”并选择 .sln 文件
+
+byte* voidByteArray(const int length) {
+	byte* rtn = new byte[length];
+	memset(rtn, 0x00, length);
+	return rtn;
+}
+
+SecByteBlock HexDecodeString(const char *hex)
+{
+	StringSource ss(hex, true, new HexDecoder);
+	SecByteBlock result((size_t)ss.MaxRetrievable());
+	ss.Get(result, result.size());
+	return result;
+}
+
+SecByteBlock HexDecode(const byte* byteArray, const int len) {
+	SecByteBlock result(byteArray, len);
+	return result;
+}
+
+string AES_CTR_Encrypt(const char *hexKey, const char *input)
+{
+	string rtn_buffer;
+	SecByteBlock key = HexDecodeString(hexKey);
+	SecByteBlock iv = HexDecode(voidByteArray(8), 8);
+	CTR_Mode<AES>::Encryption aes(key, key.size(), iv);
+	StreamTransformationFilter stf(aes, new StringSink(rtn_buffer));
+	stf.Put((byte*)input, strlen(input));
+	stf.MessageEnd();
+	return rtn_buffer;
+}
---
+++ ConsoleApplication2/ConsoleApplication2.cpp
@@ -0,0 +1,93 @@
+﻿// ConsoleApplication2.cpp : 此文件包含 "main" 函数。程序执行将在此处开始并结束。
+//
+
+#include "pch.h"
+#include <iostream>
+
+#include "aes.h"
+#include "filters.h"
+#include "secblock.h"
+#include "hex.h"
+#include "modes.h"
+#include "osrng.h"
+
+using namespace std;
+using namespace CryptoPP;
+
+//final
+byte* voidByteArray(const int length) {
+	byte* rtn = new byte[length];
+	memset(rtn, 0x00, length);
+	return rtn;
+}
+
+byte* hash(char* txt) {
+	size_t messageLen = std::strlen(txt) + 1;
+	byte sha_out[16];
+	memset(sha_out, 0x00, 16);
+	SHA256 sha;
+	sha.CalculateDigest(sha_out, (byte*)txt, messageLen);
+	return sha_out;
+}
+
+void encode(byte* hash_key, byte* input) {
+	SecByteBlock key(hash_key, 16);
+	SecByteBlock iv(voidByteArray(16), 16);
+	size_t messageLen = std::strlen((char*)input) + 1;
+	CBC_Mode<AES>::Encryption cfbEncryption(key, key.size(), iv);
+	cfbEncryption.ProcessData(input, input, messageLen);
+}
+
+void decode(byte* hash_key, byte* input) {
+	SecByteBlock key(hash_key, 16);
+	SecByteBlock iv(voidByteArray(16), 16);
+	size_t messageLen = std::strlen((char*)input) + 1;
+	CBC_Mode<AES>::Decryption cfbDecryption(key, key.size(), iv);
+	cfbDecryption.ProcessData(input, input, messageLen);
+}
+
+byte* HexDecode(const string str) {
+	string decoded;
+	auto cstr = (byte*)str.c_str();
+
+	HexDecoder decoder;
+	decoder.Put((byte*)str.data(), str.size());
+	decoder.MessageEnd();
+
+	word64 size = decoder.MaxRetrievable();
+	if (size&& size <= SIZE_MAX)
+	{
+		decoded.resize(size);
+		decoder.Get((byte*)&decoded[0], decoded.size());
+	}
+	return (byte*)decoded.data();
+}
+
+string HexEncode(const byte* barray) {
+	string encoded;
+
+	HexEncoder encoder;
+	encoder.Put(barray, strlen((char*)barray));
+	encoder.MessageEnd();
+
+	word64 size = encoder.MaxRetrievable();
+	if (size)
+	{
+		encoded.resize(size);
+		encoder.Get((byte*)&encoded[0], encoded.size());
+	}
+	return encoded;
+}
+
+
+int main()
+{
+	byte plainText[] = "Hello! How are you.";
+
+	encode(voidByteArray(16), plainText);
+	cout << HexEncode(plainText) << endl;
+	decode(voidByteArray(16), plainText);
+	cout << plainText << endl;
+
+
+}
